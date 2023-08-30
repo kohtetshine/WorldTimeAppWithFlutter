90450bfbf71b16e3eb097e0df9d47b93d270b955
Aries <shinehtet.has99@gmail.com>
2023-08-30 15:14:43 +0700

    folderstructure

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'pages/home.dart';
+import 'pages/choose_location.dart';
+import 'pages/loading.dart';
+
+
+void main() {
+  runApp(const MaterialApp(
+    home: Home(),
+  ));
+}
+
+
+
